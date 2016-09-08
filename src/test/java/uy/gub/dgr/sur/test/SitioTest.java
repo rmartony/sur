fe33df6da48683e43f038470e582dfa1fad1d97a
rmartony <rmartony@gmail.com>
2016-09-08 03:02:24 -0300

    Primeros pasos arquillian

--- src/test/java/uy/gub/dgr/sur/test/SitioTest.java
+++ src/test/java/uy/gub/dgr/sur/test/SitioTest.java
@@ -1,15 +1,22 @@
 package uy.gub.dgr.sur.test;
 
 import lombok.extern.slf4j.Slf4j;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.jboss.shrinkwrap.resolver.api.maven.Maven;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import uy.gub.dgr.sur.entity.Registro;
-import uy.gub.dgr.sur.entity.Zona;
+import uy.gub.dgr.sur.service.RegistroService;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
+import javax.inject.Inject;
+import java.io.File;
 
 /**
  * User: rafa
@@ -17,15 +24,35 @@ import javax.persistence.EntityTransaction;
  * Time: 12:22 AM
  */
 @Slf4j
+@RunWith(Arquillian.class)
 public class SitioTest {
     /**
      * The factory that produces entity manager.
      */
-    private static EntityManagerFactory mEmf;
+    //private static EntityManagerFactory mEmf;
     /**
      * The entity manager that persists and queries the DB.
      */
-    private static EntityManager em;
+    //private static EntityManager em;
+
+    @Inject
+    private RegistroService registroService;
+
+    @Deployment
+    public static Archive<?> createDeployment() {
+        File[] files = Maven.resolver().loadPomFromFile("pom.xml")
+                .importRuntimeDependencies().resolve().withTransitivity().asFile();
+
+        return ShrinkWrap.create(WebArchive.class, "test.war")
+                .addPackages(true, "uy")
+                .addAsLibraries(files)
+                .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml")
+                .addAsWebInfResource(EmptyAsset.INSTANCE, "sur-ds.xml")
+                .addAsWebInfResource(EmptyAsset.INSTANCE, "faces-config.xml")
+                .addAsWebInfResource(EmptyAsset.INSTANCE, "jboss-web.xml")
+                .setWebXML(new File("src/test/webapp/WEB-INF/web.xml"))
+                .addAsResource("META-INF/persistence.xml");
+    }
 
     @BeforeClass
     public static void initTestFixture() throws Exception {
@@ -47,15 +74,8 @@ public class SitioTest {
 */
     }
 
+/*
     @Test
-    public void pruebaRegistro() {
-        Registro s = new Registro();
-        s.setCodigo("cod1");
-        s.setDescripcion("Prueba");
-        em.persist(s);
-    }
-
-    //@Test
     public void pruebaUsuario() {
         log.info("+++++++ Persistence test ++++++++++++++");
         //Get a new transaction
@@ -70,6 +90,7 @@ public class SitioTest {
         Zona z2 = new Zona();
         z2.setNombre("zona 2");
         em.persist(z2);
+*/
 /*
         Usuario u1 = new Usuario();
         u1.setNombre("Pepito");
@@ -86,8 +107,19 @@ public class SitioTest {
         u1.setClave("juan");
         u2.setZonas(new HashSet<Zona>(Arrays.asList(z1)));
         em.persist(u1);
-*/
+*//*
+
         //Commit and end the transaction
         trx.commit();
+    }
+*/
+
+    @Test
+    public void should_create_greeting() {
+        Registro s = new Registro();
+        s.setCodigo("cod1");
+        s.setDescripcion("Prueba");
+
+        s = registroService.update(s);
     }
 }
